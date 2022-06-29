e221966d6c1505fb1a926be0ebe2eee7d495cac0
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-06-29 19:49:48 +0900

    remove nnecessary `Option`

--- src-tauri/src/dependencies/notes.rs
+++ src-tauri/src/dependencies/notes.rs
@@ -3,83 +3,54 @@ use crate::dependencies::plugins;
 use std::collections::{HashMap, HashSet};
 
 pub struct Notes {
-    notes: Option<HashMap<String, Note>>,
+    notes: HashMap<String, Note>,
 }
 
 impl Notes {
     pub fn new() -> Self {
         Notes {
-            notes: Some(HashMap::new()),
+            notes: HashMap::new(),
         }
     }
 
     pub fn append_url(&mut self, url: &str) {
-        match self.notes.as_mut() {
-            Some(notes) => {
-                let mut note = Note::default();
-                note.url = url.to_string();
-                note.need_fetch = true;
-                notes.insert(note.url.clone(), note);
-            }
-            None => {
-                println!("None None");
-            }
-        }
+        let mut note = Note::default();
+        note.url = url.to_string();
+        note.need_fetch = true;
+        self.notes.insert(note.url.clone(), note);
     }
 
     pub fn append(&mut self, note: Note) {
-        match self.notes.as_mut() {
-            Some(notes) => {
-                notes.insert(note.url.clone(), note);
-            }
-            None => {
-                println!("None None");
-            }
-        }
+        self.notes.insert(note.url.clone(), note);
     }
 
     pub fn referenced_from(&mut self, link: String, from: String) {
-        match self.notes.as_mut() {
-            Some(notes) => {
-                notes
-                    .entry(link.clone())
-                    .or_insert(Note {
-                        url: link.clone(),
-                        title: String::from(""),
-                        sources: Vec::new(),
-                        links: HashSet::new(),
-                        referenced: HashSet::new(),
-                        need_fetch: true,
-                    })
-                    .referenced_from(from);
-            }
-            None => {}
-        }
+        self.notes
+            .entry(link.clone())
+            .or_insert(Note {
+                url: link.clone(),
+                title: String::from(""),
+                sources: Vec::new(),
+                links: HashSet::new(),
+                referenced: HashSet::new(),
+                need_fetch: true,
+            })
+            .referenced_from(from);
     }
 
     pub fn analyze(&mut self) -> bool {
         // collect links
-        match self.notes.as_mut() {
-            Some(notes) => {
-                for note in notes.iter_mut() {
-                    note.1.collect_links();
-                }
-            }
-            None => {}
+        for note in self.notes.iter_mut() {
+            note.1.collect_links();
         }
 
         // create reference list
         let mut pairs: Vec<(String, String)> = Vec::new();
-        match self.notes.as_ref() {
-            Some(notes) => {
-                for note in notes.values() {
-                    let links = note.links.clone();
-                    for link in links.iter() {
-                        pairs.push((link.clone(), note.url.clone()));
-                    }
-                }
+        for note in self.notes.values() {
+            let links = note.links.clone();
+            for link in links.iter() {
+                pairs.push((link.clone(), note.url.clone()));
             }
-            None => {}
         }
 
         // apply reference
@@ -90,46 +61,42 @@ impl Notes {
         // fetch dirty note
         let mut fetch_request_count = 0;
         let mut fetched_notes: Vec<Note> = Vec::new();
-        match self.notes.as_mut() {
-            Some(notes) => {
-                let needs_fetch_notes = notes
-                    .values()
-                    .filter(|&n| n.need_fetch && n.title.is_empty());
-                for note in needs_fetch_notes {
-                    println!("what is {}", note.url);
-                    if plugins::trello::trello_match(&note.url) {
-                        let card_id = note.url.split("/").collect::<Vec<&str>>()[4];
-                        println!("need fetch as trello {} card_id: {}", note.url, card_id);
-                        fetch_request_count += 1;
-                        match plugins::trello::trello_fetch_note(&note) {
-                            Ok(note) => {
-                                fetched_notes.push(note);
-                            }
-                            Err(e) => println!("{}", e),
-                        }
-                    } else if plugins::github::github_match(&note.url) {
-                        println!("need fetch as github {}", note.url);
-                        fetch_request_count += 1;
-                        match plugins::github::github_fetch_note(&note) {
-                            Ok(note) => {
-                                fetched_notes.push(note);
-                            }
-                            Err(e) => println!("{}", e),
-                        }
-                    } else if plugins::slack::slack_match(&note.url) {
-                        println!("need fetch as slack {}", note.url);
-                        match plugins::slack::slack_fetch_note(&note) {
-                            Ok(note) => {
-                                fetched_notes.push(note);
-                            }
-                            Err(e) => println!("{}", e),
-                        }
-                    } else {
-                        // println!("unknown url {}", note.url);
+        let needs_fetch_notes = self
+            .notes
+            .values()
+            .filter(|&n| n.need_fetch && n.title.is_empty());
+        for note in needs_fetch_notes {
+            println!("what is {}", note.url);
+            if plugins::trello::trello_match(&note.url) {
+                let card_id = note.url.split("/").collect::<Vec<&str>>()[4];
+                println!("need fetch as trello {} card_id: {}", note.url, card_id);
+                fetch_request_count += 1;
+                match plugins::trello::trello_fetch_note(&note) {
+                    Ok(note) => {
+                        fetched_notes.push(note);
                     }
+                    Err(e) => println!("{}", e),
                 }
+            } else if plugins::github::github_match(&note.url) {
+                println!("need fetch as github {}", note.url);
+                fetch_request_count += 1;
+                match plugins::github::github_fetch_note(&note) {
+                    Ok(note) => {
+                        fetched_notes.push(note);
+                    }
+                    Err(e) => println!("{}", e),
+                }
+            } else if plugins::slack::slack_match(&note.url) {
+                println!("need fetch as slack {}", note.url);
+                match plugins::slack::slack_fetch_note(&note) {
+                    Ok(note) => {
+                        fetched_notes.push(note);
+                    }
+                    Err(e) => println!("{}", e),
+                }
+            } else {
+                // println!("unknown url {}", note.url);
             }
-            None => {}
         }
 
         println!(
@@ -150,21 +117,15 @@ impl Notes {
     pub fn to_string(&self) -> String {
         let notes = self
             .notes
-            .as_ref()
-            .unwrap()
             .values()
             .map(|n| n.to_string())
             .collect::<Vec<String>>()
             .join("\n");
-        format!(
-            "notes count {}\n{}",
-            self.notes.as_ref().unwrap().len(),
-            notes
-        )
+        format!("notes count {}\n{}", self.notes.len(), notes)
     }
 
     pub fn dump(&self) {
-        for note in self.notes.as_ref().unwrap().values() {
+        for note in self.notes.values() {
             note.dump();
         }
     }
