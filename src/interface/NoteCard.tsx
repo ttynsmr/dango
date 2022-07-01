5f05905f31a4926ac907f00ab698df285789811d
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-07-01 23:04:05 +0900

    adjust layout and link design

--- src/interface/NoteCard.tsx
+++ src/interface/NoteCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const NoteCard: React.FC<Props> = ({ note }) => (
   <>
-    <div className="bg-purple-50 flex flex-col rounded m-0 p-2 justify-self-auto">
+    <div className="bg-purple-50 flex flex-col rounded min-w-[150px] m-0 p-2 justify-self-auto">
       <div className="flex gap-2 p-0">
         <div className="truncate min-w-[90px] bg-purple-400 text-purple-50 rounded px-2">
           <p className={"pr-1 fa-brands fa-" + note.plugin?.toLowerCase()} />{note.plugin}
@@ -16,7 +16,10 @@ const NoteCard: React.FC<Props> = ({ note }) => (
           {note.title}
         </button>
       </div>
-      <a className="truncate tracking-tight font-sans text-purple-600 text-xs p-0" href={note.url} target="_blank">{note.url}</a>
+      <a className="truncate tracking-tight font-sans text-purple-600 text-xs p-0 hover:underline" href={note.url} target="_blank">
+        <p className="pr-1 fa-solid fa-arrow-up-right-from-square" />
+        {note.url}
+      </a>
     </div>
   </>
 );
