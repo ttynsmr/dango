98eaed6ed9b30c48c99836361714e548842d6c9a
Tetsuya Nishimura <ttynsmr@gmail.com>
2022-07-13 13:43:03 +0900

    handle Trello error

--- src/models/TrelloPlugin.ts
+++ src/models/TrelloPlugin.ts
@@ -34,25 +34,32 @@ export class TrelloPlugin implements NotePlugin {
     //https://trello.com/c/LgGUaRMO
     let splitted = url.split('/')
     let shortLink = splitted[4]
-    let response = await fetch(`https://api.trello.com/1/cards/${shortLink}?key=${this.apiKey}&token=${this.apiToken}&checklists=all`, {
+    let response = await fetch<TrelloCard>(`https://api.trello.com/1/cards/${shortLink}?key=${this.apiKey}&token=${this.apiToken}&checklists=all`, {
       method: "GET",
       headers: {
         Accept: "application/json",
       }
     });
 
-    let card: TrelloCard = response.data as TrelloCard
-
     let note = new Note()
     note.plugin = this.name
-    note.title = card.name;
-    note.url = card.shortUrl;
-    note.sources.push(card.desc)
     note.needFetch = false
 
-    note.sources.map(value => {
-      return Array.from(linkify.find(value, "url")).map(link => { return link.href })
-    }).flat().forEach(value => { note.links.add(value) })
+    if (response.ok) {
+      let card: TrelloCard = response.data
+
+      note.title = card.name;
+      note.url = card.shortUrl;
+      note.sources.push(card.desc)
+
+      note.sources.map(value => {
+        return Array.from(linkify.find(value, "url")).map(link => { return link.href })
+      }).flat().forEach(value => { note.links.add(value) })
+    }
+    else {
+      note.url = this.getNormalizedUrl(url)
+      note.title = "🔒";
+    }
 
     return note
   }
